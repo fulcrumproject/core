77423b313c02a26afa3c05e73c23116749739985
Ernesto Colombo <ernesto.colombo@contactlab.com>
2025-03-12 15:48:19 +0100

    re #14 transactions

--- internal/domain/store.go
+++ internal/domain/store.go
@@ -2,15 +2,9 @@ package domain
 
 import "context"
 
-<<<<<<< HEAD
-type WriteStore interface {
-	// Transactional callback
-	Atomic(context.Context, func(WriteStore) error) error
-=======
 type Store interface {
 	// Transactional callback
 	Atomic(context.Context, func(Store) error) error
->>>>>>> 14-transactions
 
 	// Repositories
 	AgentTypeRepo() AgentTypeRepository
