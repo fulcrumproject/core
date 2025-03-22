f6aa47c70ec54f56b5af6921725194289034344d
Ernesto Colombo <ernesto.colombo@contactlab.com>
2025-03-22 09:07:18 +0100

    mocks

---
+++ internal/mock/mock_authorizer.go
@@ -0,0 +1,33 @@
+package mock
+
+import (
+	"context"
+
+	"fulcrumproject.org/core/internal/domain"
+)
+
+type MockAuthorizer struct {
+	ShouldSucceed bool
+}
+
+var _ domain.Authorizer = (*MockAuthorizer)(nil)
+
+func (m *MockAuthorizer) Authorize(identity domain.AuthIdentity, subject domain.AuthSubject, action domain.AuthAction, scope *domain.AuthScope) error {
+	if !m.ShouldSucceed {
+		return domain.NewUnauthorizedErrorf("mock authorization failed")
+	}
+	return nil
+}
+
+func (m *MockAuthorizer) AuthorizeCtx(ctx context.Context, subject domain.AuthSubject, action domain.AuthAction, scope *domain.AuthScope) error {
+	if !m.ShouldSucceed {
+		return domain.NewUnauthorizedErrorf("mock authorization failed")
+	}
+	return nil
+}
+
+func NewMockAuthorizer(shouldSucceed bool) *MockAuthorizer {
+	return &MockAuthorizer{
+		ShouldSucceed: shouldSucceed,
+	}
+}
