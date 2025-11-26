bee69faeb0472641b874593913b98c3bee50e538
giammarcoferranti <giammarco.ferranti@testudosrl.com>
2025-11-26 12:47:35 +0100

    Feature/new keycloak json (#101)

--- pkg/app/api_server.go
+++ pkg/app/api_server.go
@@ -11,6 +11,7 @@ import (
 	"github.com/fulcrumproject/utils/logging"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/go-chi/render"
 )
 
@@ -103,6 +104,19 @@ func BuildHttpServer(
 ) *http.Server {
 	// Initialize router
 	r := chi.NewRouter()
+
+	// Basic CORS
+  // for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+  r.Use(cors.Handler(cors.Options{
+    // AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+    AllowedOrigins:   []string{"https://*", "http://*"},
+    // AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+    AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+    ExposedHeaders:   []string{"Link"},
+    AllowCredentials: false,
+    MaxAge:           300, // Maximum value not ignored by any of major browsers
+  }))
 
 	// Middleware
 	r.Use(
